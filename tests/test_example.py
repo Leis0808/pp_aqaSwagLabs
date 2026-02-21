de80f70fc59bdd25f2a78208dd7dfc7fa01d99e5
Leis0808 <nasypova.leisan@gmail.com>
2026-02-21 23:44:38 +0400

    add some tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -30,6 +30,26 @@ def test_login_locked_out_user(page: Page):
     result = page.locator("//div[contains(@class, 'error')]")
     expect(result).to_have_text("Epic sadface: Sorry, this user has been locked out.")
 
+def test_negative_login(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.locator('input[data-test="username"]').fill("user")
+    page.locator('input[data-test="password"]').fill("secret_sauce")
+    page.get_by_role('button', name='Login').click()
+
+    result = page.locator("//div[contains(@class, 'error')]")
+    expect(result).to_have_text("Epic sadface: Username and password do not match any user in this service")
+
+def test_negative_password(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.locator('input[data-test="username"]').fill("standard_user")
+    page.locator('input[data-test="password"]').fill("secret")
+    page.get_by_role('button', name='Login').click()
+
+    result = page.locator("//div[contains(@class, 'error')]")
+    expect(result).to_have_text("Epic sadface: Username and password do not match any user in this service")
+
 
 
 def test_add_products(page: Page):
@@ -62,3 +82,64 @@ def test_remove_products(page: Page):
     shopping = page.locator('//a[@data-test="shopping-cart-link"]')
     expect(shopping).not_to_have_text("1")
 
+def test_logout(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.locator('input[data-test="username"]').fill("standard_user")
+    page.locator('input[data-test="password"]').fill("secret_sauce")
+    page.get_by_role('button', name='Login').click()
+
+    open_menu = page.get_by_text("Open Menu")
+    open_menu.click()
+
+    loguot = page.locator('#logout_sidebar_link')
+    loguot.click()
+
+    expect(page).to_have_url("https://www.saucedemo.com/")
+
+
+def test_goto_shopping(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.locator('input[data-test="username"]').fill("standard_user")
+    page.locator('input[data-test="password"]').fill("secret_sauce")
+    page.get_by_role('button', name='Login').click()
+
+    product = page.locator("(//button[contains(text(), 'Add to cart')])[1]")
+    product.click()
+
+    shopping = page.locator(".shopping_cart_link")
+    shopping.click()
+
+    checkout = page.locator("#checkout")
+    checkout.click()
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-step-one.html")
+
+
+def test_end2end(page: Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.locator('input[data-test="username"]').fill("standard_user")
+    page.locator('input[data-test="password"]').fill("secret_sauce")
+    page.get_by_role('button', name='Login').click()
+
+    product = page.locator("(//button[contains(text(), 'Add to cart')])[1]")
+    product.click()
+
+    shopping = page.locator(".shopping_cart_link")
+    shopping.click()
+
+    checkout = page.locator("#checkout")
+    checkout.click()
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-step-one.html")
+    page.get_by_placeholder("First Name").fill("Ivan")
+    page.get_by_placeholder('Last Name').press_sequentially("Petrov")
+    page.get_by_placeholder("Zip/Postal Code").type("Moscow, 12345")
+
+    page.get_by_role("button", name="Continue").click()
+
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-step-two.html")
+    page.get_by_role("button", name="finish").click()
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-complete.html")
+    page.get_by_role("button", name="Back Home").click()
+    expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
